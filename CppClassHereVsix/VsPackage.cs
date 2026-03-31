c0293eb44ad627741c020982b80695d870532c96
강명호 <3044965@gmail.com>
2026-03-31 23:03:06 +0900

    Avoid startup autoload for package

--- CppClassHereVsix/VsPackage.cs
+++ CppClassHereVsix/VsPackage.cs
@@ -1,25 +1,45 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 
 namespace CppClassHereVsix
 {
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-    [ProvideAutoLoad(UIContextGuids80.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
-    [ProvideAutoLoad(UIContextGuids80.SolutionExists, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideMenuResource("Menus.CTMENU", 1)]
     [Guid(PackageGuidString)]
     public sealed class VsPackage : AsyncPackage
     {
         public const string PackageGuidString = "f0434c21-891e-4dd4-b097-ce71dda1cd08";
+        private static readonly string LogPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CppClassHereVsix.Package.log");
 
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-            await AddCppClassHereCommand.InitializeAsync(this);
+            try
+            {
+                AppendLog("InitializeAsync start");
+                await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+                await AddCppClassHereCommand.InitializeAsync(this);
+                AppendLog("InitializeAsync success");
+            }
+            catch (Exception ex)
+            {
+                AppendLog("InitializeAsync failure: " + ex);
+                throw;
+            }
+        }
+
+        private static void AppendLog(string message)
+        {
+            try
+            {
+                File.AppendAllText(LogPath, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + " " + message + Environment.NewLine);
+            }
+            catch
+            {
+            }
         }
     }
 }
